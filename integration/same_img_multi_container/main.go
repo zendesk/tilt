be89fef2f07a72c336b2ebe1896154db96a07b40
Maia <maia@windmill.engineering>
2019-08-06 12:41:43 -0400

    breaking stuff

--- integration/same_img_multi_container/main.go
+++ integration/same_img_multi_container/main.go
@@ -4,23 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 import "flag"
 
 var port = flag.Int("port", 9999, "port to run server on")
 
+var shouldPanic bool
+
 func main() {
 	flag.Parse()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		msg := "🍄 One-Up! 🍄"
+		msg := "🍄 mushroomtime! 🍄"
 		log.Printf("Got HTTP request for %s", r.URL.Path)
 		_, _ = w.Write([]byte(msg))
+		time.Sleep(200 * time.Millisecond)
+		shouldPanic = true
 	})
 
 	log.Printf("Serving oneup on container port %d\n", *port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
-	if err != nil {
-		log.Printf("SERVER DIED WITH ERROR:\n\t%v", err)
+	go http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+	for {
+		if shouldPanic {
+			panic("egads!")
+		}
 	}
 }
