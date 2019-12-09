eef7703ebca2fe4c095238a5fb73aa2f992a108e
Dan Miller <dmiller@windmill.engineering>
2019-12-09 18:14:32 -0500

    first passing test

--- internal/engine/telemetrycontroller.go
+++ internal/engine/telemetrycontroller.go
@@ -2,13 +2,18 @@ package engine
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 	"sync"
+	"time"
 
 	"github.com/windmilleng/wmclient/pkg/dirs"
 
 	"github.com/windmilleng/tilt/internal/build"
 	"github.com/windmilleng/tilt/internal/store"
 )
+
+const AnalyticsFilename = "analytics"
 
 type TelemetryController struct {
 	clock build.Clock
@@ -26,11 +31,33 @@ func NewTelemetryController(lock sync.Locker, clock build.Clock, dir *dirs.Windm
 }
 
 func (t *TelemetryController) OnChange(ctx context.Context, st store.RStore) {
+	state := st.RLockState()
+	lastTelemetryRun := state.LastTelemetryScriptRun
+	script := state.TelemetryScriptPath
+	st.RUnlockState()
+
 	// store in the engine state the last time we sent data. If it's been more than N minutes since then:
-	//   if there's a script, exec it
-	// if it completed, dispatch action to set time
-	// (error handling)
-	//   regardless: truncate the file
+	if lastTelemetryRun.Add(1 * time.Hour).Before(t.clock.Now()) {
+		if script != "" {
+			// TODO
+		}
+		// if there's a script, exec it, passing in the contents of the file
+		// if it completed, dispatch action to set time
+		// TODO(dmiller): error handling
+
+		// regardless: truncate the file
+		err := t.truncateAnalyticsFile()
+		if err != nil {
+			// TODO(dmiller): error handling
+		}
+	}
+}
+
+func (t *TelemetryController) truncateAnalyticsFile() error {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	return os.Truncate(filepath.Join(t.dir.Root(), AnalyticsFilename), 0)
 }
 
 // TODO(dbentley) file based span exporter
