07ed36781b25d0a7b505c17dfb21b5ad536994b9
Dan Bentley <dan@tilt.dev>
2019-12-10 08:55:32 -0500

    trace to file

--- internal/engine/telemetrycontroller.go
+++ internal/engine/telemetrycontroller.go
@@ -4,13 +4,13 @@ import (
 	"context"
 	"os"
 	"path/filepath"
-	"sync"
-	"time"
+	// "time"
 
 	"github.com/windmilleng/wmclient/pkg/dirs"
 
 	"github.com/windmilleng/tilt/internal/build"
 	"github.com/windmilleng/tilt/internal/store"
+	"github.com/windmilleng/tilt/internal/tracer"
 )
 
 const AnalyticsFilename = "analytics"
@@ -18,11 +18,11 @@ const AnalyticsFilename = "analytics"
 type TelemetryController struct {
 	clock build.Clock
 	// lock is a lock that is used to control access from the span exporter and the telemetry controller to the file
-	lock sync.Locker
+	lock tracer.Locker
 	dir  *dirs.WindmillDir
 }
 
-func NewTelemetryController(lock sync.Locker, clock build.Clock, dir *dirs.WindmillDir) *TelemetryController {
+func NewTelemetryController(lock tracer.Locker, clock build.Clock, dir *dirs.WindmillDir) *TelemetryController {
 	return &TelemetryController{
 		lock:  lock,
 		clock: clock,
@@ -38,14 +38,17 @@ func (t *TelemetryController) OnChange(ctx context.Context, st store.RStore) {
 
 	// store in the engine state the last time we sent data. If it's been more than N minutes since then:
 	if lastTelemetryRun.Add(1 * time.Hour).Before(t.clock.Now()) {
-		if script != "" {
-			// TODO
+		if script == "" {
+			return
 		}
-		// if there's a script, exec it, passing in the contents of the file
-		// if it completed, dispatch action to set time
+		t.lock.Lock()
+		defer t.lock.Unlock()
+		// exec the script, passing in the contents of the file
+
+		// dispatch action to set time
 		// TODO(dmiller): error handling
 
-		// regardless: truncate the file
+		// truncate the file if the script succeeded
 		err := t.truncateAnalyticsFile()
 		if err != nil {
 			// TODO(dmiller): error handling
@@ -54,10 +57,5 @@ func (t *TelemetryController) OnChange(ctx context.Context, st store.RStore) {
 }
 
 func (t *TelemetryController) truncateAnalyticsFile() error {
-	t.lock.Lock()
-	defer t.lock.Unlock()
-
-	return os.Truncate(filepath.Join(t.dir.Root(), AnalyticsFilename), 0)
+	return dir.WriteFile(tracer.OutgoingFilename, "")
 }
-
-// TODO(dbentley) file based span exporter
