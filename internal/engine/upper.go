f21d39b9f29a0d466a6eae1f7256531f9bf9c5c9
Maia <maia@windmill.engineering>
2019-12-13 16:40:44 -0500

    calculate available build slots from max build slots, instead of tracking it separately

--- internal/engine/upper.go
+++ internal/engine/upper.go
@@ -241,7 +241,6 @@ func handleBuildStarted(ctx context.Context, state *store.EngineState, action bu
 	}
 	ms.ConfigFilesThatCausedChange = []string{}
 	ms.CurrentBuild = bs
-	state.BuildControllerSlotsAvailable -= 1
 
 	if ms.IsK8s() {
 		for _, pod := range ms.K8sRuntimeState().Pods {
@@ -269,7 +268,6 @@ func handleBuildCompleted(ctx context.Context, engineState *store.EngineState, c
 
 	buildCount := engineState.CompletedBuildCount
 	engineState.CompletedBuildCount++
-	engineState.BuildControllerSlotsAvailable++
 
 	mt, ok := engineState.ManifestTargets[cb.ManifestName]
 	if !ok {
