e4f4efae4400355105b731ff9b0ba1157e007336
Maia <maia@windmill.engineering>
2020-08-05 17:39:33 -0400

    wip

--- internal/hud/server/logs_reader_test.go
+++ internal/hud/server/logs_reader_test.go
@@ -9,11 +9,16 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/tilt-dev/tilt/pkg/model"
+
 	"github.com/tilt-dev/tilt/internal/hud"
 	proto_webview "github.com/tilt-dev/tilt/pkg/webview"
 )
 
-var defaultMessages = []string{"alpha", "bravo", "charlie", "delta"}
+var alphabet = []string{"alpha", "bravo", "charlie", "delta", "echo", "foxtrot",
+	"golf", "hotel", "igloo", "juliette", "kilo", "lima", "mike", "november",
+	"oscar", "papa", "quebec", "romeo", "sierra", "tango", "uniform", "victor",
+	"whiskey", "xavier", "yankee", "zulu"}
 
 type expectedLine struct {
 	prefix  string // ~manifestName (leave blank for "global" / unprefixed)
@@ -22,10 +27,11 @@ type expectedLine struct {
 
 func TestLogStreamerPrintsLogs(t *testing.T) {
 	f := newFixture(t)
-	view := f.newViewWithLogsForManifest(defaultMessages, "foo")
+
+	view := f.newViewWithLogsForManifest(alphabet[:4], "foo")
 	f.handle(view)
 
-	expected := f.expectedLinesWithPrefix(defaultMessages, "foo")
+	expected := f.expectedLinesWithPrefix(alphabet[:4], "foo")
 
 	f.assertExpectedLogLines(expected)
 }
@@ -33,28 +39,69 @@ func TestLogStreamerPrintsLogs(t *testing.T) {
 func TestLogStreamerPrefixing(t *testing.T) {
 	f := newFixture(t)
 	manifestNames := []string{"foo", "", "foo", "bar"}
-	view := f.newViewWithLogsForManifests(defaultMessages, manifestNames)
+
+	view := f.newViewWithLogsForManifests(alphabet[:4], manifestNames)
 	f.handle(view)
 
-	expected := f.expectedLinesWithPrefixes(defaultMessages, manifestNames)
+	expected := f.expectedLinesWithPrefixes(alphabet[:4], manifestNames)
 
 	f.assertExpectedLogLines(expected)
 }
 
 func TestLogStreamerDoesNotPrintResentLogs(t *testing.T) {
 	f := newFixture(t)
-	view := f.newViewWithLogsForManifest(defaultMessages, "foo")
+
+	view := f.newViewWithLogsForManifest(alphabet[:4], "foo")
 	f.handle(view)
 
-	view.LogList.Segments = append(view.LogList.Segments, &proto_webview.LogSegment{
-		SpanId: spanID("foo"),
-		Text:   "echo",
-	})
-	view.LogList.ToCheckpoint = int32(len(view.LogList.Segments))
+	f.addLogs(view, alphabet[4:5], []string{"foo"})
 	f.handle(view)
 
-	expected := f.expectedLinesWithPrefix(append(defaultMessages, "echo"), "foo")
+	expected := f.expectedLinesWithPrefix(alphabet[:5], "foo")
 
+	f.assertExpectedLogLines(expected)
+}
+
+func TestLogStreamerFiltersOnResourceNamesSingle(t *testing.T) {
+	f := newFixture(t).withResourceNames("foo")
+	manifestNames := []string{"foo", "", "foo", "bar"}
+	view := f.newViewWithLogsForManifests(alphabet[:4], manifestNames)
+	f.handle(view)
+
+	// Expect no prefix b/c we're filtering for a single resource, so prefixing is redundant
+	expected := f.expectedLinesWithPrefix([]string{"alpha", "charlie"}, "")
+	f.assertExpectedLogLines(expected)
+}
+
+func TestLogStreamerFiltersOnResourceNamesMultiple(t *testing.T) {
+	f := newFixture(t).withResourceNames("foo", "baz")
+	manifestNames := []string{"foo", "", "foo", "bar", "baz", "bar", "baz", "foo"}
+	view := f.newViewWithLogsForManifests(alphabet[:8], manifestNames)
+	f.handle(view)
+
+	expected := f.expectedLinesWithPrefixes(
+		[]string{"alpha", "charlie", "echo", "golf", "hotel"}, []string{"foo", "foo", "baz", "baz", "foo"})
+	f.assertExpectedLogLines(expected)
+}
+
+func TestLogStreamerWithFilteringHandlesCheckpoints(t *testing.T) {
+	// Check that the way we track internal vs. external checkpoints doesn't break
+	f := newFixture(t).withResourceNames("foo", "baz")
+	view := f.newViewWithLogsForManifests(alphabet[:4], []string{"foo", "", "foo", "bar"})
+	f.handle(view)
+
+	view = f.newViewWithLogsForManifests(alphabet[4:8], []string{"bar", "bar", "", "bar"})
+	view.LogList.FromCheckpoint = 4
+	view.LogList.ToCheckpoint = 8
+	f.handle(view)
+
+	view = f.newViewWithLogsForManifests(alphabet[8:12], []string{"", "foo", "bar", "baz"})
+	view.LogList.FromCheckpoint = 8
+	view.LogList.ToCheckpoint = 12
+	f.handle(view)
+
+	expected := f.expectedLinesWithPrefixes(
+		[]string{"alpha", "charlie", "juliette", "lima"}, []string{"foo", "foo", "foo", "baz"})
 	f.assertExpectedLogLines(expected)
 }
 
@@ -72,9 +119,19 @@ func newFixture(t *testing.T) *fixture {
 		t:          t,
 		fakeStdout: fakeStdout,
 		printer:    printer,
-		ls:         NewLogStreamer(printer),
+		ls:         NewLogStreamer(nil, printer),
 	}
 }
+
+func (f *fixture) withResourceNames(resourceNames ...string) *fixture {
+	rns := make(model.ManifestNameSet, len(resourceNames))
+	for _, rn := range resourceNames {
+		rns[model.ManifestName(rn)] = true
+	}
+	f.ls.resources = rns
+	return f
+}
+
 func (f *fixture) handle(view proto_webview.View) {
 	err := f.ls.Handle(view)
 	require.NoError(f.t, err)
@@ -89,8 +146,32 @@ func (f *fixture) newViewWithLogsForManifest(messages []string, manifestName str
 }
 
 func (f *fixture) newViewWithLogsForManifests(messages []string, manifestNames []string) proto_webview.View {
+	segs := f.segments(messages, manifestNames)
+	spans := f.spans(manifestNames, nil)
+
+	return proto_webview.View{
+		LogList: &proto_webview.LogList{
+			Spans:          spans,
+			Segments:       segs,
+			FromCheckpoint: 0,
+			ToCheckpoint:   int32(len(segs)),
+		},
+	}
+}
+
+func (f *fixture) addLogs(view proto_webview.View, messages []string, manifestNames []string) proto_webview.View {
+	newSegs := f.segments(messages, manifestNames)
+	view.LogList.Segments = append(view.LogList.Segments, newSegs...)
+	view.LogList.ToCheckpoint = int32(len(view.LogList.Segments))
+
+	view.LogList.Spans = f.spans(manifestNames, view.LogList.Spans)
+
+	return view
+}
+
+func (f *fixture) segments(messages []string, manifestNames []string) []*proto_webview.LogSegment {
 	if len(messages) != len(manifestNames) {
-		f.t.Fatalf("Need same number of prefixes and manifestNames (got %d and %d)",
+		f.t.Fatalf("Need same number of messages and manifestNames (got %d and %d)",
 			len(messages), len(manifestNames))
 	}
 
@@ -102,19 +183,22 @@ func (f *fixture) newViewWithLogsForManifests(messages []string, manifestNames [
 			Level:  0, // TODO
 		}
 	}
-	spans := make(map[string]*proto_webview.LogSpan)
-	for _, mn := range manifestNames {
-		spans[spanID(mn)] = &proto_webview.LogSpan{ManifestName: mn}
+
+	return segs
+}
+
+func (f *fixture) spans(manifestNames []string,
+	existingSpans map[string]*proto_webview.LogSpan) map[string]*proto_webview.LogSpan {
+
+	if existingSpans == nil {
+		existingSpans = make(map[string]*proto_webview.LogSpan)
 	}
 
-	return proto_webview.View{
-		LogList: &proto_webview.LogList{ // TODO: be better
-			Spans:          spans,
-			Segments:       segs,
-			FromCheckpoint: 0,
-			ToCheckpoint:   int32(len(segs)),
-		},
+	for _, mn := range manifestNames {
+		existingSpans[spanID(mn)] = &proto_webview.LogSpan{ManifestName: mn}
 	}
+
+	return existingSpans
 }
 
 func (f *fixture) assertExpectedLogLines(expectedLines []expectedLine) {
